f77bf2717b3f62d43b59a43d9368e26ebe9b17d7
Tim Long <Tim.Long@oceansignal.com>
2024-06-10 09:34:51 +0100

    Don't use file-scoped namespaces in .net 5/C#9

--- test/AisParserTests/MessagesTests/SafetyRelatedBroadcastMessageTests.cs
+++ test/AisParserTests/MessagesTests/SafetyRelatedBroadcastMessageTests.cs
@@ -3,21 +3,22 @@ using AisParser.Messages;
 using Shouldly;
 using Xunit;
 
-namespace AisParserTests.MessagesTests;
-
-public class SafetyRelatedBroadcastMessageTests : MessageTestBase
+namespace AisParserTests.MessagesTests
 {
-    [Fact]
-    public void Should_parse_message()
+    public class SafetyRelatedBroadcastMessageTests : MessageTestBase
     {
-        const string sentence = "!AIVDM,1,1,,A,>>QK2khE0U8:1@E=@0,4*52"; // EPIRB test message
+        [Fact]
+        public void Should_parse_message()
+        {
+            const string sentence = "!AIVDM,1,1,,A,>>QK2khE0U8:1@E=@0,4*52"; // EPIRB test message
 
-        var message = Parser.Parse(sentence) as SafetyRelatedBroadcastMessage;
-        message.ShouldNotBeNull();
-        message.MessageType.ShouldBe(AisMessageType.SafetyRelatedBroadcastMessage);
-        message.Repeat.ShouldBe(0u);
-        message.Mmsi.ShouldBe(974570191u);
-        message.Spare.ShouldBe(0u);
-        message.SafetyRelatedText.ShouldBe("EPIRB TEST");
+            var message = Parser.Parse(sentence) as SafetyRelatedBroadcastMessage;
+            message.ShouldNotBeNull();
+            message.MessageType.ShouldBe(AisMessageType.SafetyRelatedBroadcastMessage);
+            message.Repeat.ShouldBe(0u);
+            message.Mmsi.ShouldBe(974570191u);
+            message.Spare.ShouldBe(0u);
+            message.SafetyRelatedText.ShouldBe("EPIRB TEST");
+        }
     }
 }
